bc55442c99360da45bb590d65dd115d6ed9ccd23
chrissy <christopherchristopherclark@gmail.com>
2019-03-28 13:53:14 -0700

    more obvious submit button

--- src/sentry/static/sentry/app/components/organizations/headerItem.jsx
+++ src/sentry/static/sentry/app/components/organizations/headerItem.jsx
@@ -28,22 +28,12 @@ class HeaderItem extends React.Component {
     this.props.onClear();
   };
 
-  handleChevronClick = e => {
-    if (!this.props.hasChanges) {
-      return;
-    }
-
-    e.stopPropagation();
-    this.props.onSubmit();
-  };
-
   render() {
     const {
       className,
       children,
       isOpen,
       hasSelected,
-      hasChanges,
       allowClear,
       icon,
       locked,
@@ -76,11 +66,7 @@ class HeaderItem extends React.Component {
             />
           )}
         {!locked && (
-          <StyledChevron
-            isOpen={isOpen}
-            hasChanges={hasChanges}
-            onClick={this.handleChevronClick}
-          >
+          <StyledChevron isOpen={isOpen}>
             <InlineSvg src="icon-chevron-down" />
           </StyledChevron>
         )}
@@ -145,17 +131,6 @@ const StyledChevron = styled('div')`
   display: flex;
   align-items: center;
   justify-content: center;
-  ${p =>
-    p.hasChanges
-      ? `
-    background: ${p.theme.purple};
-    border-radius: 2em;
-    width: 20px;
-    height: 20px;
-    color: #fff;
-    transform: rotate(270deg);
-  `
-      : ''};
 `;
 
 const StyledLock = styled(InlineSvg)`
